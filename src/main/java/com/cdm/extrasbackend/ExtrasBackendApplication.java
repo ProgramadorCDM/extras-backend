be2f65dc9196d7240fe8f2d5fbcaa5d2f62b50ba
Kenyodarz <minajorge@hotmail.com>
2020-03-09 08:01:36 -0500

    App Web para el control de horas extras procesando los datos generado por la app JavaFX "HorarioFX".

---
+++ src/main/java/com/cdm/extrasbackend/ExtrasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cdm.extrasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtrasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtrasBackendApplication.class, args);
+    }
+
+}
