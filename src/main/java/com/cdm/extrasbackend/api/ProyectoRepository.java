0a33438dbd22d2f172158da963a37adf6803f0e3
Kenyodarz <minajorge@hotmail.com>
2020-03-17 08:16:04 -0500

    Version 0.0.0.4

--- src/main/java/com/cdm/extrasbackend/api/ProyectoRepository.java
+++ src/main/java/com/cdm/extrasbackend/api/ProyectoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProyectoRepository extends JpaRepository <Proyecto, Long> {
+public interface ProyectoRepository extends JpaRepository <Proyecto, String> {
 }
